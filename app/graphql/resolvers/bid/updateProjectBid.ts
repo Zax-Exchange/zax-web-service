81d31f3e15111a4323d1e44214c03b6455594cba
Joseph Cheng <josephcheng@Josephs-MacBook-Pro.local>
2022-08-27 16:21:13 -0700

    refactor

--- app/graphql/resolvers/bid/updateProjectBid.ts
+++
@@ -1,9 +0,0 @@
-// import { updateProjectBid as updateProjectBidApi } from "../../../../api/project/updateProjectApis";
-// import { UpdateProjectBidInput } from "../../../../api/types/update/projectTypes";
-
-// const updateProjectBid = (parent: any, args: Record<string, UpdateProjectBidInput>, context: any, info: any) => {
-
-//   return updateProjectBidApi(args.data);
-// };
-
-// export default updateProjectBid;
