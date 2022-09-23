5e533126c23b4ff11e6c43c81400c0e27116fc1e
William Sun <sunwilliam@gmail.com>
2022-09-22 18:44:49 -0700

    added code to determine project changes

--- app/graphql/resolvers/project/update/updateProject.resolver.ts
+++ app/graphql/resolvers/project/update/updateProject.resolver.ts
@@ -1,9 +1,31 @@
+import { projects, projectsAttributes } from "../../../../models/projects";
 import sequelize from "../../../../postgres/dbconnection";
 import streamService from "../../../../stream/StreamService";
 import {
   BidStatus,
   UpdateProjectInput,
 } from "../../../resolvers-types.generated";
+
+const determineProjectDiff = (originalEntity: projects, projectUpdateData: UpdateProjectInput ) => {
+  console.debug('originalmodel=', originalEntity);
+  console.debug('updatedata=', projectUpdateData);
+  var output: any[] = [];
+  Object.entries(projectUpdateData)
+    .filter(([k,v]) => k !== 'projectId') // projectId is not changeable but is in UpdateProjectInput
+    .forEach(([key, value]) =>  {
+      const originalValue = originalEntity.getDataValue(key as keyof projectsAttributes);
+      // perform a deep comparison
+      if (JSON.stringify(value) !== JSON.stringify(originalValue)) {
+        output.push({
+          key,
+          old: originalValue,
+          new: value
+        });
+      }
+    }
+  );
+  return output;
+}
 
 // TODO: broadcast update project even to vendors
 const updateProject = async (
@@ -24,6 +46,9 @@ const updateProject = async (
   } = data;
   try {
     await sequelize.transaction(async (transaction) => {
+      const originalModel: projects = await sequelize.models.projects.findByPk(projectId) as projects;
+      const changes = determineProjectDiff( originalModel, data);
+      console.log('changes=', changes);
       await Promise.all([
         sequelize.models.projects.update(
           {
