01682e08b40f749550545b5e64fcaf2f23406c92
Joseph Cheng <josephcheng@MacBook-Pro-2.local>
2023-02-15 20:47:27 -0800

    added config files for sequelize migration

---
+++ config/config.cjs
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    dialect: "postgres",
+  },
+  stage: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    dialect: "postgres",
+  },
+};
