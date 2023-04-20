bcf84d1459523e08d2f466ac9f25d11946b73f12
Joseph Cheng <josephcheng@MacBook-Pro-2.local>
2023-04-20 16:10:26 -0700

    delete files when deleting projects

--- app/graphql/resolvers/project/delete/deleteProject.resolver.ts
+++ app/graphql/resolvers/project/delete/deleteProject.resolver.ts
@@ -2,6 +2,7 @@ import ElasticProjectService from "../../../../elastic/project/ElasticProjectSer
 import sequelize from "../../../../postgres/dbconnection";
 import cacheService from "../../../../redis/CacheService";
 import { DeleteProjectInput } from "../../../resolvers-types.generated";
+import { deleteProjectDesign } from "./deleteProjectDesign.resolver";
 
 const deleteProject = async (
   parent: any,
@@ -12,11 +13,30 @@ const deleteProject = async (
   const projects = sequelize.models.projects;
   const { projectId } = data;
   try {
+    const allDesigns = await sequelize.models.project_designs
+      .findAll({
+        where: {
+          projectId,
+        },
+      })
+      .then((files) => files.map((file) => file.get("id") as string));
+
+    await Promise.all(
+      allDesigns.map((fileId) => {
+        return deleteProjectDesign(null, {
+          data: {
+            fileId,
+          },
+        });
+      })
+    );
+
     await projects.destroy({
       where: {
         id: projectId,
       },
     });
+
     ElasticProjectService.deleteProjectDocument(projectId);
     await cacheService.invalidateProjectInCache(projectId);
     return Promise.resolve(true);
